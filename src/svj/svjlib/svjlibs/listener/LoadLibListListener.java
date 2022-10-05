c71d365449317e2905bd1fb0487021e3c6d79d94
svj <sergey.zhiganov@eltex.loc>
2022-10-05 17:52:35 +0700

    load lib files. step-1

--- src/svj/svjlib/svjlibs/listener/LoadLibListListener.java
+++ src/svj/svjlib/svjlibs/listener/LoadLibListListener.java
@@ -1,11 +1,16 @@
 package svj.svjlib.svjlibs.listener;
 
+import svj.svjlib.Log;
 import svj.svjlib.gui.dialog.WidgetsDialog;
 import svj.svjlib.gui.widget.FileWidget;
 import svj.svjlib.gui.widget.StringFieldWidget;
+import svj.svjlib.obj.BookTitle;
+import svj.svjlib.tools.DialogTools;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.*;
 
 /**
  * Загрузить новую библиотеку.
@@ -49,10 +54,42 @@ public class LoadLibListListener implements ActionListener {
 
         if (dialog.isOK()) {
 
-            // Проверяем парва дсотупа к директории. Если что - запрашиваем логин-пароль на доступ.
+            String libName = myLibName.getValue();
+            // Берем директорию
+            String libDir = dirPath.getValue();
+
+            // todo Проверяем права дсотупа к директории. Если что - запрашиваем логин-пароль на доступ.
+
+            // Par.BOOKS - список всех книг - туда добавить если есть данные
+
+            try {
+                // сам процесс чтения todo Здесь необходим бегунок с процентом рабоыт - по кол-ву файлов в библиотеке
+                Collection<BookTitle> result = processLoad(libDir);
+
+                // todo сохранить инфу о новой библиотеке в конфиг-директории Проги
+
+            } catch (Exception e) {
+                Log.l.error("libDir = " + libDir, e);
+                DialogTools.showError("Ошибка загрузки библиотеки '" + libDir + "' :\n " + e.getMessage(), "Ошибка загрузки");
+            }
 
         }
 
     }
-    
+
+    private Collection<BookTitle> processLoad(String libDir) {
+
+        File dirFile = new File(libDir);
+        String[] list = dirFile.list();
+
+        if (list != null)
+            Log.l.info("list size = {}", list.length);
+        else
+            Log.l.info("list files is Null");
+
+        throw new RuntimeException("list = " + list);
+
+        //return null;
+    }
+
 }
